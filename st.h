bc11c7c955da5677a6cb5ece962ab0599941f392
Aviral Goel <goel.aviral@gmail.com>
2018-05-29 10:47:12 -0400

    Add patch for opening clipboard contents (url) in browser
    Patch obtained from https://st.suckless.org/patches/open_copied_url/

--- st.h
+++ st.h
@@ -80,6 +80,8 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
+void iso14755(const Arg *);
+void opencopied(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
