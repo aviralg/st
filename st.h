c9a4635cdca45f04bd21945a40a9a05d42a70ba0
Aviral Goel <goel.aviral@gmail.com>
2019-08-17 16:52:09 +0200

    Apply iso14755 patch
    https://st.suckless.org/patches/iso14755/st-iso14755-20180911-67d0cb6.diff

--- st.h
+++ st.h
@@ -81,6 +81,7 @@ void redraw(void);
 void draw(void);
 
 void opencopied(const Arg *);
+void iso14755(const Arg *);
 void printscreen(const Arg *);
 void printsel(const Arg *);
 void sendbreak(const Arg *);
